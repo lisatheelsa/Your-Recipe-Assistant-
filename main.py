a67dd1f8caefe462ac92dd4dfab104d3a90612cd
Greta Donskova <gretadonskova@gmail.com>
2024-06-01 00:43:59 +0300

    Добавлена функция генерации рандомного рецепта

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ def save_user_info(user):
 cursor.execute('''CREATE TABLE IF NOT EXISTS messages
                   (message_id INTEGER PRIMARY KEY, user_id INTEGER, message_text TEXT, sent_at TEXT)''')
 
-
 def save_message(message):
     message_id = message.message_id
     user_id = message.from_user.id
@@ -66,6 +65,45 @@ def send_welcome(message, rerun=0):
     bot.send_message(message.chat.id, "Введите через запятую Ваши аллергены. Например: семечки, яблоко, "
                                       "капуста")
 
+def get_rec(chat_id, id):
+    bot.send_message(chat_id,
+                     f"<b>Кухня:</b> {recipes.iloc[id, 0]}\n"
+                     f"\n<b>Название блюда:</b> {recipes.iloc[id, 1]}\n"
+                     f"\n<b>Ингредиенты:</b> {recipes.iloc[id, 2]}\n"
+                     f"\n<b>Время готовки:</b> {recipes.iloc[id, 3]}\n"
+                     f"\n<b>Калории:</b> {recipes.iloc[id, 4]}\n"
+                     f"\n<b>и сам рецепт)):</b> {recipes.iloc[id, 5]}", parse_mode='html'
+                     )
+
+def get_similar_rec(chat_id, dish):
+    vectorizer = TfidfVectorizer()
+
+    # Преобразуйте текстовые данные в матрицу TF-IDF
+    tfidf_matrix = vectorizer.fit_transform(recipes['Название блюда'])
+
+    # Преобразуйте введенное слово в вектор TF-IDF
+    word_vector = vectorizer.transform([dish])
+
+    # Вычислите косинусное сходство между вектором введенного слова и всеми векторами блюд
+    cosine_similarities = cosine_similarity(word_vector, tfidf_matrix).flatten()
+
+    # Найдите индексы блюд с косинусным сходством на 50% и больше
+    similar_indices = [i for i, similarity in enumerate(cosine_similarities) if similarity >= 0.5]
+    final_similarity_indices = []
+
+    for index in similar_indices:
+        flag = True
+        for recipe in recipes.iloc[index, 2]:
+            if recipe not in all_allergens:
+                flag = False
+        if flag:
+            final_similarity_indices.append(index)
+
+# Выведите названия блюд со сходством на 50% и больше
+    bot.send_message(chat_id, f"По вашему запросу было найдено {len(final_similarity_indices)} блюд:")
+    for index in final_similarity_indices:
+        get_rec(chat_id, index)
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
@@ -74,24 +112,29 @@ def echo_all(message):
     save_message(message)
     # bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAELxNVl_I_YzLIlvZcNl5jT_M7iEUvBigACVhMAAvNbAUhsonT0E7AAAeQ0BA")
 
-    # сохранение аллергенов в бдшку
-    allergens = message.text
-    all_allergens = allergens.split(',')
-    bot.send_message(message.chat.id,
-                     f"Ваши аллергены {allergens.split(',')} были сохранены. Если хотите их изменить, "
-                     f"просто напиши их заново")
+    if message_lower in 'рандом':
+        random_rec = random.randint(0, len(recipes) - 1)
+        get_rec(message.chat.id, random_rec)
+        bot.send_message(message.chat.id, "Для нового случайного рецепта напиши слово  <b>рандом</b>\n\n"
+                                          "Для поиска блюда напиши 'хочу Название блюда'", parse_mode='html')
+    else:
+        # сохранение аллергенов в бдшку
+        allergens = message.text
+        all_allergens = allergens.split(',')
+        bot.send_message(message.chat.id,
+                         f"Ваши аллергены {allergens.split(',')} были сохранены. Если хотите их изменить, "
+                         f"просто напиши их заново")
 
-    with db_lock:
-        cursor.execute("UPDATE users SET allergen = ? WHERE user_id = ?", (allergens, message.from_user.id))
-        conn.commit()
+        with db_lock:
+            cursor.execute("UPDATE users SET allergen = ? WHERE user_id = ?", (allergens, message.from_user.id))
+            conn.commit()
 
-    # после аллергенов челику можно рекомендовать то что он хочет
-    bot.send_message(message.chat.id,
-                     "Теперь ты можешь выбрать что будешь кушать\n\n"
-                     "Для случайного рецепта напишите слово <b>рандом</b>\n\n"
-                     "Для поиска блюда напиши 'хочу Название блюда' (в именительном падеже,"
-                     "единственном числе)", parse_mode='html')
-
+        # после аллергенов челику можно рекомендовать то что он хочет
+        bot.send_message(message.chat.id,
+                         "Теперь ты можешь выбрать что будешь кушать\n\n"
+                         "Для случайного рецепта напишите слово <b>рандом</b>\n\n"
+                         "Для поиска блюда напиши 'хочу Название блюда' (в именительном падеже,"
+                         "единственном числе)", parse_mode='html')
 
 
 bot.infinity_polling()
